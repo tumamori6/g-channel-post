7a957f3826bcd77430d562adf6295acf0df85564
tumamori6 <tumamori6@gmail.com>
2021-12-10 09:17:50 +0900

    debug

--- tw_module.js
+++ tw_module.js
@@ -16,25 +16,24 @@ exports.updatePost = async(post_text) => {
 	await page.waitForSelector('.twitter_login_button_w');
 	await page.click('.twitter_login_button_w');
 	await page.waitForTimeout(2000);
+	await page.waitForSelector('[name="session[username_or_email]"]');
+	await page.click('[name="session[username_or_email]"]');
+	await page.keyboard.type(process.env.USER_ID);
+	await page.waitForSelector('[name="session[password]"]');
+	await page.click('[name="session[password]"]');
+	await page.keyboard.type(process.env.USER_PASS);
+	await Promise.all([
+		page.waitForNavigation({ waitUntil: ['load', 'networkidle2'], timeout: 0 }),
+		page.click('.submit.button.selected'),
+	]);
+	await page.waitForTimeout(5000);
 
-	//heroku ipでメール認証 初回のみ
-	const html = await page.$eval('body', item => {
-		return item.innerHTML;
-	});
-	console.log(html);
-
-	// await page.waitForSelector('[name="session[username_or_email]"]');
-	// await page.click('[name="session[username_or_email]"]');
-	// await page.keyboard.type(process.env.USER_ID);
-	// await page.waitForSelector('[name="session[password]"]');
-	// await page.click('[name="session[password]"]');
-	// await page.keyboard.type(process.env.USER_PASS);
-	// await Promise.all([
-	// 	page.waitForNavigation({ waitUntil: ['load', 'networkidle2'], timeout: 0 }),
-	// 	page.click('.submit.button.selected'),
-	// ]);
-	// await page.waitForTimeout(5000);
-
+		//heroku ipでメール認証 初回のみ
+		const html = await page.$eval('body', item => {
+			return item.innerHTML;
+		});
+		console.log(html);
+	
 	// await page.goto('https://metabirds.net/admin/bot_random.php');
 	// await page.waitForSelector('#message_1');
 	// await page.focus('#message_1');
